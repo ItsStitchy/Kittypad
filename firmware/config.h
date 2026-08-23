b753c5100bd4c695385977110cdee22af17b3eb1
ItsStitchy <85883079+ItsStitchy@users.noreply.github.com>
2026-08-23 23:07:30 +0800

    Remove unused pin definitions from config.h

--- firmware/config.h
+++ firmware/config.h
@@ -1,15 +1,3 @@
 #pragma once
 
-
-#define DIRECT_PINS { \
-    { D10, D9, D8, D7, D2 } \
-}
-
-
-#define ENCODERS_PAD_A { D0 }
-#define ENCODERS_PAD_B { D1 }
 #define ENCODER_RESOLUTION 4
-
-
-#define WS2812_DI_PIN D6
-#define RGBLIGHT_LED_COUNT 1
